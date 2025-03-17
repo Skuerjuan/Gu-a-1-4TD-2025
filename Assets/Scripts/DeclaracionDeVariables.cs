dbde8deb8f47bd5d73515de913c3dace140fa4f6
skuerjuan <maierowiczfelipe@gmail.com>
2025-03-17 10:41:57 -0300

    inicializar variables y loggearlas

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class DeclaracionDeVariables : MonoBehaviour
 {
+    //declarando variables
     int edadUsuario;
     float sueldoUsuario;
     string domicilio;
@@ -12,7 +14,18 @@ public class DeclaracionDeVariables : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //inicializando variables
+        edadUsuario = 45;
+        sueldoUsuario = 1000.00f;
+        domicilio = "Libertador";
+        estaAprobado = true;
+
+        //loggeando valores
+        UnityEngine.Debug.Log(edadUsuario);
+        UnityEngine.Debug.Log(sueldoUsuario);
+        UnityEngine.Debug.Log(domicilio);
+        UnityEngine.Debug.Log(estaAprobado);
+
     }
 
     // Update is called once per frame
