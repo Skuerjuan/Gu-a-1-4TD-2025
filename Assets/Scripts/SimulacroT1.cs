d8f964715cf821dd2b5a73ffc5cc302d7eee1a39
skuerjuan <maierowiczfelipe@gmail.com>
2025-04-07 10:38:46 -0300

    Simulacros de Examen Tema N°1 y N°3 Resueltos

---
+++ Assets/Scripts/SimulacroT1.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulacroT1 : MonoBehaviour
+{
+    public float montoInicial;
+    public char moneda;
+    float montoFinal;
+    string monedaEntera;
+
+    float montoMinimo = 1000;
+    float cotizacion;
+
+    float cotizacionDolar = 1073.63f;
+    float cotizacionEuro = 1179.4f;
+    float cotizacionReal = 183.75f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (montoInicial < montoMinimo)
+        {
+            Debug.Log($"El monto mínimo es $1000.");
+            return;
+        }
+
+        switch (moneda)
+        {
+            case 'D':
+                cotizacion = cotizacionDolar;
+                monedaEntera = "Dólar(es)";
+                break;
+            case 'R':
+                cotizacion = cotizacionReal;
+                monedaEntera = "Real(es)";
+                break;
+            case 'E':
+                cotizacion = cotizacionEuro;
+                monedaEntera = "Euro(s)";
+                break;
+            default:
+                Debug.Log("Opción de moneda extranjera no válida.");
+                return;
+        }
+
+        montoFinal = montoInicial / cotizacion;
+
+
+        Debug.Log($"${montoInicial} pesos son iguales a {montoFinal} {monedaEntera}.");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SimulacroT3.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulacroT3 : MonoBehaviour
+{
+    public string codigoAnimal;
+    public int diasEstadia;
+
+
+    int comidaDiaria;
+    int comidaConsumida;
+    float precioTotal;
+
+    float precioAlimento = 80;
+    int comidaDiariaG = 300;
+    int comidaDiariaPP = 400;
+    int comidaDiariaPG = 700;
+    int diasMinimos = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (diasEstadia < diasMinimos)
+        {
+            Debug.Log($"La cantidad mínima de días para una estadía es {diasMinimos}.");
+            return;
+        }
+
+        switch (codigoAnimal)
+        {
+            case "G":
+                comidaDiaria = comidaDiariaG;
+                break;
+
+            case "PP":
+                comidaDiaria = comidaDiariaPP;
+                break;
+
+            case "PG":
+                comidaDiaria = comidaDiariaPG;
+                break;
+
+            default:
+                Debug.Log("Debe ingresar un código de animal válido.");
+                return;
+        }
+
+        comidaConsumida = comidaDiaria * diasEstadia;
+        precioTotal = (precioAlimento / 100) * comidaConsumida;
+
+        Debug.Log($"Para ese período es necesitan {comidaConsumida} gramos de alimento.");
+        Debug.Log($"El precio total de la comida será de ${precioTotal}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
